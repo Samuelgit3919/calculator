48d1cb6e9409bea269d42b287740a94164eb89c3
samuel sami <saminew3919@gmail.com>
2024-06-13 06:54:59 -0700

    commit change

---
+++ To-Do-List/src/App.jsx
@@ -0,0 +1,11 @@
+import Todo from "./components/Todo"
+
+function App() {
+  return (
+    <div className="bg-stone-600 grid py-4 min-h-screen">
+      <Todo />
+    </div>
+  )
+}
+
+export default App
